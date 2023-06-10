9c19475b7d42113fd55882c46ec96b05cb16aba8
Hansani Gunathilaka <121996526+Gunathi@users.noreply.github.com>
2023-06-10 20:50:37 +0530

    Add files via upload

---
+++ Pattern 2/Pattern2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main(){
+
+    int i,j,a=0;
+
+    for(i=0; i<5; i++){
+        for(j=0; j<=a; j++){
+            printf("*");
+        }
+        printf("\n");
+        a++;
+    }
+
+    printf("\n\n");
+
+    for(i=0; i<5; i++){
+        for(j=5; j>=a; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    a--;
+}
