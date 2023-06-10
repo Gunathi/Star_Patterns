c848f36adda0c16978ad651292ae8f5d0fd8fbf5
Hansani Gunathilaka <121996526+Gunathi@users.noreply.github.com>
2023-06-10 20:53:42 +0530

    Add files via upload

---
+++ Pattern 3/Pattern3.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+
+void main() {
+
+    int i,j,k,rows;
+
+    printf("Enter number of rows do you want: ");
+    scanf("%d",&rows);
+
+    //Right Half Pyramid
+    for(i=0; i<rows; i++){
+        for(j=0; j<=i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+
+    /*  *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        * * * * * * ...
+    */
+
+    //Left Half Pyramid
+    for(i=0; i<rows; i++){
+        for(j=rows; j>=i; j--){
+            printf("  ");
+        }
+        for(k=0; k<=i; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    /*           *
+               * *
+             * * *
+           * * * *
+         * * * * *
+    */
+
+
+    //Full Pyramid
+    for(i=0; i<rows; i++){
+        for(j=0; j<=rows-i; j++){
+            printf(" ");
+        }
+        for(k=0; k<=i; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    /*      *
+           * *
+          * * *
+         * * * *
+        * * * * *
+
+    */
+
+
+    //Full pyramid second type
+    for(i=0; i<rows; i++){
+        for(j=0; j<=rows-i-2; j++){
+            printf("  ");
+        }
+        for(k=0; k<2*i+1; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    /*      *
+          * * *
+        * * * * *
+      * * * * * * *
+    * * * * * * * * *
+
+
+    */
+
+}
